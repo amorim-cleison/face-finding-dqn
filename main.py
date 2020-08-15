5220764a275dd598c44b848c5f59ed060b41bcfb
Cleison Correia de Amorim <cleison.c.amorim@accenture.com>
2020-08-15 01:35:29 -0300

    Improving progress log on console

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ def main():
     dqn_agent = DQN(env=env)
 
     for trial in range(trials):
-        # cur_state = env.reset().reshape(1, 2)
+        print_start(trial)
+
         cur_state = env.reset()
 
         for step in range(trial_len):
@@ -20,7 +21,7 @@ def main():
             new_state, reward, done = env.step(action)
             env.render()
             # reward = reward if not done else -20
-            print(reward)
+            print_progress(step, cur_state, action, reward)
             # new_state = new_state.reshape(1, 2)
             dqn_agent.remember(cur_state, action, reward, new_state, done)
 
@@ -30,12 +31,33 @@ def main():
             if done:
                 break
         if step >= (trial_len - 1):
-            print("Failed to complete trial")
+            print_end(False, trials)
         else:
-            print("Completed in {} trials".format(trial))
-            # break
+            print_end(True, trials)
+
+
+def print_start(trial):
+    print("=" * 60)
+    print(f"TRIAL {(trial+1)}")
+    print("-" * 60)
+    print(f"{'Step':5} | {'State':<25}\t {'Action'}\t {'Reward'}")
+    print("-" * 60)
+
+
+def print_progress(step, state, action, reward):
+    print(
+        f"{(step+1):<5} | S: {hash(str(state)):<25}\t A: {action}\t R: {reward:.2f}"
+    )
+
+
+def print_end(success, trials):
+    print("-" * 60)
+    if success:
+        print(f" -> Completed in {trials} trials")
+    else:
+        print(" -> Failed to complete trial")
+    print()
 
 
 if __name__ == "__main__":
     main()
-rnd_pointrnd_pointrnd_pointpointrnd_norm_pointrnd_norm_pointrnd_norm_pointnorm_pointrnd_centerrnd_centerrnd_centermin_centermin_centermax_centermax_centercentercenterscalenew_statenew_statenew_viewnew_viewnew_view
